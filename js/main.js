65d4229f2859352e7292370abcb5e8a5af331c6a
Arnel <nelly02d@gmail.com>
2023-03-03 14:23:32 -0500

    Add funtions to keypad

--- js/main.js
+++ js/main.js
@@ -46,7 +46,100 @@ const drumSound = {
   }
 }
 
+//Query Selector
+
 const soundName = document.querySelector('#beat-name');
 const drumPad = document.querySelectorAll('.drum-pad');
 const volume = document.querySelector('#volume-control');
 
+for(let drum in drumSound) {
+  
+  const keyBoard = drumSound[drum].key; //get key value on drum sound nested object
+  const soundLink = drumSound[drum].src; //get audio source value on drum sound nested object
+  const name = drumSound[drum].name; //get name value on drum sound nested object
+  const audio = new Audio(soundLink); //create audio constructor
+
+  //Click Event Function
+  for(let i = 0; i < drumPad.length; i++) {
+    drumPad[i].addEventListener('click', event => {
+      if (drum == drumPad[i].id) {
+        event.target.classList.add('animate'); //add the animation
+        setTimeout(() => event.target.classList.remove('animate'), 300); //remove the animation
+        audio.currentTime = 0; //set time delay for audio
+        audio.play(); //play the current audio
+        soundName.innerHTML = name; //set equivalent sound output name when numpad is pressed
+      };
+    });
+
+    //KeyBoard Event Function
+    window.addEventListener('keydown', event => {
+      const numKeys = event.keyCode; //get keyboard key code
+      
+      drumPad.forEach(pad => {
+        if(drum === pad.id && numKeys === keyBoard) {
+          audio.currentTime = 0; //set time delay for audio
+          audio.play(); //play the current audio
+          soundName.innerHTML = name; //set equivalent sound output name when numpad is pressed
+          pad.classList.add('animate'); //add the animation
+          setTimeout(() => pad.classList.remove('animate'), 300); //remove the animation
+        };
+      });
+    });
+  };
+
+  // Volume Function
+  volume.addEventListener('change', event => {
+    audio.volume = event.currentTarget.value / 100;
+  })
+};
+
+
+//keyboard function
+// const keyboardSound = event => {
+//   const numKeys = event.keyCode; //get keyboard key code
+//   console.log(numKeys)
+  // for(let drum in drumSound) {
+  //   const keyBoard = drumSound[drum].key; //get key value on drum sound nested object
+  //   const soundLink = drumSound[drum].src; //get audio source value on drum sound nested object
+  //   const name = drumSound[drum].name; //get name value on drum sound nested object
+  //   const audio = new Audio(soundLink); //create audio constructor
+
+  //   drumPad.forEach(pad => {
+  //     if(drum === pad.id && numKeys === keyBoard) {
+  //       audio.currentTime = 0; //set time delay for audio
+  //       audio.play(); //play the current audio
+  //       soundName.innerHTML = name; //set equivalent sound output name when numpad is pressed
+  //       pad.classList.add('animate'); //add the animation
+  //       setTimeout(() => pad.classList.remove('animate'), 300); //remove the animation
+  //     };
+  //   });
+  // };
+// };
+
+// //Click event function
+// for(let i = 0; i < drumPad.length; i++) {
+//   drumPad[i].addEventListener('click', event => {
+//     for(let drum in drumSound) {
+//       const keyBoard = drumSound[drum].key; //get key value on drum sound nested object
+//       const soundLink = drumSound[drum].src; //get audio source value on drum sound nested object
+//       const name = drumSound[drum].name; //get name value on drum sound nested object
+//       const audio = new Audio(soundLink); //create audio constructor
+      
+//       if (drum == drumPad[i].id) {
+//         event.target.classList.add('animate'); //add the animation
+//         setTimeout(() => event.target.classList.remove('animate'), 300); //remove the animation
+//         audio.currentTime = 0; //set time delay for audio
+//         audio.play(); //play the current audio
+//         soundName.innerHTML = name; //set equivalent sound output name when numpad is pressed
+//       };
+//     };
+//   });
+// };
+
+//Volume Function
+// const volumeControl = event => {
+//   audio.volume = e.currentTarget.value / 100;
+// };
+
+
+// window.addEventListener('keydown', keyboardSound);
